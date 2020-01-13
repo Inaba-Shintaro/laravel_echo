393ffb57e3056fad3ef0f9d99b0bc4da28cf974f
inaba <inaba.shintaro.6221@gmail.com>
2020-01-13 20:37:59 +0700

    to change database name

---
+++ resources/js/components/TodoListComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h1>Todoリスト</h1>
+                <ul>
+                <li v-for="todo in todos">{{ todo['name'] }}</li>
+                </ul>
+                <input type="text" v-model="newTodo" @blur="addTodo">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                todos : [],
+                newTodo : ''
+            }
+        },
+        mounted(){
+            axios.get('/api/todos').then(response => (this.todos = response.data));  
+
+            window.Echo.channel('todo-added-channel')
+            .listen('TodoAdded',response => {
+                this.todos.push(response.todo);
+            });   
+
+        },
+        methods:{
+        addTodo(){
+            axios.post('/api/todos',{
+                name : this.newTodo,
+            })
+            .then(response => this.todos.push(response.data));         
+
+        this.newTodo = '';
+
+            }
+        }
+    }
+</script>
